c580e4d6474329c6f23d9f1efdfa6ec6d72eb170
Jeff Brown Tech <jeff@jeffbrown.tech>
2020-05-28 22:20:22 -0500

    working on comment help and shouldprocess

--- AzureM365GroupManagement/AzureM365GroupManagement.psm1
+++ AzureM365GroupManagement/AzureM365GroupManagement.psm1
@@ -1,7 +1,37 @@
 # https://docs.microsoft.com/microsoft-365/admin/create-groups/manage-creation-of-groups
 
 function Set-M365GroupCreationAllowedGroup {
-    [CmdletBinding(DefaultParameterSetName = 'GroupName')]
+
+    <#
+        .SYNOPSIS
+        Configures the allowed group that can create Microsoft 365 Groups.
+
+        .DESCRIPTION
+        Configures the allowed group that can create Microsoft 365 Groups. Groups are identified by ObjectId or Name.
+
+        .PARAMETER GroupName
+        The full name of the group in Azure Active Directory. (Required)
+        
+        .PARAMETER GroupId
+        The Group or ObjectId of the group in Azure Active Directory (e.g. fd4ec70a-274a-4c23-9c47-5dbc1a69c342). (Required)
+
+        .EXAMPLE
+        Set-M365GroupCreationAllowed Group -GroupName "Allowed M365 Group Creators"
+
+        This example uses the name of the group to configure the allowed group setting.
+
+        .EXAMPLE
+        Set-M365GroupCreationAllowed Group -GroupId fd4ec70a-274a-4c23-9c47-5dbc1a69c342
+
+        This example uses the group or objectId of the group to configure the allowed group setting.
+    #>
+
+    [CmdletBinding(
+        DefaultParameterSetName = 'GroupName',
+        SupportsShouldProcess,
+        ConfirmImpact = 'Medium'
+    )]
+
     param(
         [Parameter(Mandatory, ParameterSetName = 'GroupName')]
         [string]
@@ -15,6 +45,7 @@ function Set-M365GroupCreationAllowedGroup {
         Write-Warning -Message "No Group.Unified Directory Setting currently exists. Run New-GroupUnifiedDirectorySetting to create Group.Unified directory setting first."
         RETURN
     }
+
     if ($PSBoundParameters.ContainsKey("GroupName")) {
         $groupFound = Get-AzureADGroup -SearchString $GroupName
             
@@ -28,6 +59,7 @@ function Set-M365GroupCreationAllowedGroup {
             Default { Write-Warning -Message "Something else went wrong with $GroupName."; RETURN }
         }
     }
+
     if ($PSBoundParameters.ContainsKey("GroupId")) {
         try {
             $groupFound = Get-AzureADGroup -ObjectId $GroupId -ErrorAction STOP
@@ -38,41 +70,76 @@ function Set-M365GroupCreationAllowedGroup {
         }
         $groupFoundId = $groupFound.ObjectId
     }
+
     $groupUnifiedObject = Get-AzureADDirectorySetting | Where-Object -Property DisplayName -EQ -Value "Group.Unified"
     $groupUnifiedObject["GroupCreationAllowedGroupId"] = $groupFoundId
         
     try {
-        Set-AzureADDirectorySetting -Id $groupUnifiedObject.Id -DirectorySetting $groupUnifiedObject -ErrorAction STOP
-        Get-AzureADDirectorySetting -Id $groupUnifiedObject.Id | Select-Object -ExpandProperty Values
+        if ($PSCmdlet.ShouldProcess($groupFoundId)) {
+            Set-AzureADDirectorySetting -Id $groupUnifiedObject.Id -DirectorySetting $groupUnifiedObject -ErrorAction STOP
+            Get-AzureADDirectorySetting -Id $groupUnifiedObject.Id | Select-Object -ExpandProperty Values
+        }        
     }
     catch {
-        Write-Error -Message "Error enabling Group.Unified Azure AD Directory Setting: $($Error[0])"
+        Write-Error -Message "Error enabling Group.Unified Azure AD Directory Setting: $($_.Exception)"
         RETURN
     }
 } # End of Set-M365GroupCreationAllowedGroup
 
 function Remove-M365GroupCreationAllowedGroup {
-    [CmdletBinding()]
+    
+    <#
+        .SYNOPSIS
+        Clears the group setting for the group that is allowed to create Microsoft 365 Groups.
+
+        .DESCRIPTION
+        Clears the group setting for the group that is allowed to create Microsoft 365 Groups.
+
+        .EXAMPLE
+        Remove-M365GroupCreationAllowedGroup
+
+        This example clears the allowed group setting for any group configured to create Microsoft 365 Groups.
+    #>
+    
+    [CmdletBinding(
+        SupportsShouldProcess,
+        ConfirmImpact = 'Medium'
+    )]
     param()
 
     if ((Test-GroupUnifiedDirectorySetting) -eq $false) {
         Write-Warning -Message "No Group.Unified Directory Setting currently exists. No changes being made."
     }
     else {
-        $groupUnifiedObject = Get-AzureADDirectorySetting | Where-Object -Property DisplayName -Value "Group.Unified" -EQ
+        $groupUnifiedObject = Get-AzureADDirectorySetting | Where-Object -Property DisplayName -EQ -Value "Group.Unified"
+        $currentGroupId = $groupUnifiedObject.Values | Where-Object -Propert 'Name' -EQ 'GroupCreationAllowedGroupId' | Select-Object -ExpandProperty Value
         $groupUnifiedObject["GroupCreationAllowedGroupId"] = ""
+
         try {
-            Set-AzureADDirectorySetting -Id $groupUnifiedObject.Id -DirectorySetting $groupUnifiedObject -ErrorAction STOP
-            Get-AzureADDirectorySetting -Id $groupUnifiedObject.Id | Select-Object -ExpandProperty Values
+            if ($PSCmdlet.ShouldProcess($currentGroupId, 'Clearing GroupCreationAllowedGroupId')) {
+                Set-AzureADDirectorySetting -Id $groupUnifiedObject.Id -DirectorySetting $groupUnifiedObject -ErrorAction STOP
+                Get-AzureADDirectorySetting -Id $groupUnifiedObject.Id | Select-Object -ExpandProperty Values
+            }            
         }
         catch {
-            Write-Error -Message "Error enabling Group.Unified Azure AD Directory Setting: $($Error[0])"
+            Write-Error -Message "Error clearing GroupCreationAllowedGroupId Azure AD Directory Setting: $($Error[0])"
             RETURN
         }
     }
 } # End of Remove-M365GroupCreationAllowedGroup
 
 function Enable-M365GroupCreation {
+    
+    <#
+        .SYNOPSIS
+
+        .DESCRIPTION
+
+        .PARAMETER
+
+        .EXAMPLE
+    #>
+    
     [CmdletBinding()]
     param()
 
@@ -96,6 +163,17 @@ function Enable-M365GroupCreation {
 } # End of Enable-M365GroupCreation
 
 function Disable-M365GroupCreation {
+    
+    <#
+        .SYNOPSIS
+
+        .DESCRIPTION
+
+        .PARAMETER
+
+        .EXAMPLE
+    #>
+    
     [CmdletBinding()]
     param()
     
@@ -117,6 +195,17 @@ function Disable-M365GroupCreation {
 } # End of Disable-M365GroupCreation
 
 function Set-M365GroupUsageGuidelinesUrl {
+    
+    <#
+        .SYNOPSIS
+
+        .DESCRIPTION
+
+        .PARAMETER
+
+        .EXAMPLE
+    #>
+    
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
@@ -143,6 +232,17 @@ function Set-M365GroupUsageGuidelinesUrl {
 } # End of Set-M365GroupUsageGuidelinesUrl
 
 function Remove-M365GroupUsageGuidelinesUrl {
+    
+    <#
+        .SYNOPSIS
+
+        .DESCRIPTION
+
+        .PARAMETER
+
+        .EXAMPLE
+    #>
+    
     [CmdletBinding()]
     param()
 
@@ -164,6 +264,17 @@ function Remove-M365GroupUsageGuidelinesUrl {
 } # End of Remove-M365GroupUsageGuidelinesUrl
 
 function Test-GroupUnifiedDirectorySetting {
+    
+    <#
+        .SYNOPSIS
+
+        .DESCRIPTION
+
+        .PARAMETER
+
+        .EXAMPLE
+    #>
+    
     [CmdletBinding()]
     param ()
 
@@ -172,6 +283,17 @@ function Test-GroupUnifiedDirectorySetting {
 } # End of Test-GroupUnifiedDirectorySetting
 
 function New-GroupUnifiedDirectorySetting {
+    
+    <#
+        .SYNOPSIS
+
+        .DESCRIPTION
+
+        .PARAMETER
+
+        .EXAMPLE
+    #>
+    
     [CmdletBinding()]
     param()
     Write-Verbose -Message "Creating new Azure AD Directory Setting using Group.Unified template"
@@ -181,6 +303,17 @@ function New-GroupUnifiedDirectorySetting {
 } # End of New-GroupUnifiedDirectorySetting
 
 function Remove-GroupUnifiedDirectorySetting {
+    
+    <#
+        .SYNOPSIS
+
+        .DESCRIPTION
+
+        .PARAMETER
+
+        .EXAMPLE
+    #>    
+    
     [CmdletBinding()]
     param()
 
